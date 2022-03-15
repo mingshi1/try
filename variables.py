505ddcacaa13019edecddf4ffa4f673c1daf5c50
mingshi1 <mingshi.21@intl.zju.edu.cn>
2022-03-15 16:54:10 +0800

    variables.py

--- variables.py
+++ variables.py
@@ -1,1 +1,38 @@
-print ("hello world")
+print ("hello professor")
+a=19245301  
+# cases of COVID-19 in the UK in 2022
+b=4218520
+# cases of COVID-19 in the UK in 2021
+c=271
+# cases of COVID-19 in the UK in 2020
+d=b-c
+# difference between the numbers of cases in 2020 and 2021
+print("the difference between the numbers of cases in 2020 and 2021 is ")
+print(d)
+e=a-b
+# difference between the numbers of cases in 2021 and 2022
+print("the difference between the numbers of cases in 2021 and 2022 is ")
+print(e)
+f=d>e
+print("Is the difference between the numbers of cases in 2020 and 2021 higher than cases in 2021 and 2022?")
+print(f)
+# test if "the difference between the numbers of cases in 2020 and 2021 is higher than cases in 2021 and 2022
+r1=(b-c)/c
+#  rate of new cases in 2020
+r2=(a-b)/b
+#  rate of new cases in 2021
+g=r1>r2
+print("Was the rate of new cases greater in 2020 or 2021?")
+print(g)
+# test if the rate of new cases was greater in 2020 or 2021.
+
+x="hello"
+y="world"
+w=x and y
+print(w)
+# w always equals the latter one.
+X="hello"
+Y="world"
+W=X or Y
+print(W)
+# W always equals the former one.
