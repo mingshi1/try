3ec83bf83c34c10b7f47dae4725749277e60dcb3
mingshi1 <mingshi.21@intl.zju.edu.cn>
2022-03-18 15:26:27 +0800

    pizza_party(2).py

--- pizza_party.py
+++ pizza_party.py
@@ -7,8 +7,12 @@ n=0
 p=1
 sen=''
 # determine the initial value of n and p
+# define 'sen' as the sentence we finally get
 while p<=64:
 	p=(n**2+n+2)/2
+	# find n with p
 	sen=sen+str(n)+" "
+	# add the result into the sentence
 	n=n+1
+	# repeat doing the process
 print(sen)
